4a1abbc217cccfe46f58d298e533082dae11fa09
billbo24 <wfloyd1231@gmail.com>
2025-11-10 20:39:54 -0500

    few tweaks to the other pages

--- FlaskApp.py
+++ FlaskApp.py
@@ -16,12 +16,48 @@ import os
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", os.urandom(24))
 
+#This allows every template to access the company name and doesn't need it to be recalled
+@app.context_processor
+def inject_company_name():
+    return dict(company_name="Popeyes")
+
 #This is the homepage
 @app.route('/')
 def home():
     #return render_template('gradient.html')
-    company_name = 'Poopoo Peepee'
-    return render_template('home.html', company_name = company_name)
+    main_blurb = 'Unlocking the value of your business'
+    sub_blurb = 'Optimizing businesses'
+    return render_template('home.html', 
+                           main_blurb=main_blurb,
+                           sub_blurb = sub_blurb)
+
+
+#This is the homepage
+@app.route('/automation')
+def automation():
+    #return render_template('gradient.html')
+    automation_blurb = 'Automating the repetitive tasks so you can focus on delivering value'
+    return render_template('automation.html', 
+                           content_blurb = automation_blurb)
+
+
+#This is the homepage
+@app.route('/optimization')
+def optimization():
+    #return render_template('gradient.html')
+    optimization_blurb = 'Optimizing your existing processes allowing you more time to serve your business'
+    return render_template('optimization.html', 
+                           content_blurb = optimization_blurb)
+
+
+#This is the homepage
+@app.route('/analysis')
+def analysis():
+    #return render_template('gradient.html')
+    analysis_blurb = 'Demystifying data allowing you to make powerful choices'
+    return render_template('analysis.html', 
+                           content_blurb = analysis_blurb)
+
 
 
 
